507272da54560a4afd6d52d6f1617d05fcea3a1d
jianjun.hu <jianjun.hu@quantinfotech.com>
2023-02-10 11:32:13 +0800

    捕获错误

--- src/index.ts
+++ src/index.ts
@@ -5,38 +5,71 @@ import { getAllFiles, getBlogInfo, getFileNames } from './utils/file';
 const app = express();
 const PORT = 4090;
 const staticPath =  path.resolve(process.cwd(), "./blogs");
-console.log(staticPath);
 
 app.get('/', (req, res) => {
+  console.log("result");
   res.status(200);
   res.send('Hello World!');
 });
 
 app.get('/blog/list', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200);
   console.log("访问列表");
   getFileNames(staticPath).then(result => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200);
-    res.json(result);
+    res.json({
+      statusCode: "000",
+      data: result,
+      message: null,
+    });
+  }).catch((err) => {
+    console.log("Error", err);
+    res.json({
+      statusCode: "001",
+      data: null,
+      message: `staticPath: ${staticPath} is Not Found`,
+    });
   });
 });
 
 app.get('/blog/datalist', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200);
   console.log("/blog/datalist")
   getAllFiles(staticPath).then(result => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200);
-    res.json(result);
+    res.json({
+      statusCode: "000",
+      data: result,
+      message: null,
+    });
+  }).catch((err) => {
+    console.log("Error", err);
+    res.json({
+      statusCode: "001",
+      data: null,
+      message: `staticPath: ${staticPath} is Not Found`,
+    });
   });
 });
 
 app.get('/blog/detail', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.status(200);
   const { fileName } = req.query;
   console.log(`/blog/detail?fileName=${fileName}`);
   getBlogInfo(staticPath, fileName as string).then((result) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200);
-    res.json(result);
+    res.json({
+      statusCode: "000",
+      data: result,
+      message: null,
+    });
+  }).catch((err) => {
+    console.log("Error", err);
+    res.json({
+      statusCode: "001",
+      data: null,
+      message: `FileName: ${fileName} is Not Found`,
+    });
   });
 });
 
